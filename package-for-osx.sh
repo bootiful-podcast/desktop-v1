94e3067e63a5e233886094c69537da9ee55b3004
Josh Long <josh@joshlong.com>
2019-06-30 03:10:13 -0400

    polish

--- package-for-osx.sh
+++ package-for-osx.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 
-jpackager create-image --output myoutput --name desktop-client --input target --main-jar desktop-client.jar
+jpackager create-image --output myoutput --name desktop-client --input target --main-jar desktop.jar
