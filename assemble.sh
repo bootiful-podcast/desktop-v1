e062b983521157336dcb7f8044390a01d703e91d
Josh Long <josh@joshlong.com>
2019-07-04 05:21:50 -0700

    polish..

--- assemble.sh
+++ assemble.sh
@@ -1,24 +1,19 @@
 #!/usr/bin/env bash
 
-HERE=$( cd `dirname $0` && pwd )
+HERE=$(cd `dirname $0` && pwd )
 echo $HERE
 export BP_DESKTOP=$HERE/target/package
 rm -rf ${BP_DESKTOP}
 mkdir -p ${BP_DESKTOP}
-
 mvn -DskipTests=true spring-javaformat:apply clean package
 cp ${HERE}/target/desktop.jar ${BP_DESKTOP}
 cp ${HERE}/target/run.sh ${BP_DESKTOP}
-
 cd ${BP_DESKTOP}
 ls -la
-
 APPNAME="Bootiful Podcast"
 DIR="${APPNAME}.app/Contents/MacOS"
-
 mkdir -p "${DIR}"
 cp ${HERE}/run.sh "$DIR/$APPNAME"
 cp ${HERE}/target/desktop.jar "$DIR/$APPNAME"
 chmod +x "$DIR/$APPNAME"
-
 echo "$PWD/$APPNAME.app"
