90f1ed364922b220a2d830c9517ea1325a774e39
Josh Long <josh@joshlong.com>
2019-07-04 05:41:08 -0700

    polish..

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ LOG=$HOME/Desktop/bootiful-podcast.log
 echo "starting.." > $LOG
 echo $0 >> $LOG
 echo $PWD >> $LOG
-`dirname $0`/desktop.jar >  $LOG  2>&1
+`dirname $0`/desktop.jar > $LOG 2>&1
 disown
