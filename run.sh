f0b5a8fe842b9a784f7a34d2288e6057082e6da4
Josh Long <josh@joshlong.com>
2019-07-04 18:07:06 -0700

    added an icon and i feel good.

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-export SPRING_PROFILES_ACTIVE=prod
-export JAVA_HOME=${HOME}/.sdkman/candidates/java/current
-LOG=$HOME/Desktop/bootiful-podcast.log
-echo "starting.." > $LOG
-echo $0 >> $LOG
-echo $PWD >> $LOG
-`dirname $0`/desktop.jar > $LOG 2>&1
-disown
