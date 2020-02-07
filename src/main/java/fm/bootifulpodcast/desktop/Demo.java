192c906e5ce124c16d0cedbfbcad11b0987b1422
Josh Long <josh@joshlong.com>
2020-02-06 21:25:09 -0800

    changed the sample demo data.

--- src/main/java/fm/bootifulpodcast/desktop/Demo.java
+++ src/main/java/fm/bootifulpodcast/desktop/Demo.java
@@ -21,11 +21,11 @@ class Demo {
 	@EventListener(StageReadyEvent.class)
 	public void loadSampleData() {
 		var home = System.getProperty("user.home");
-		var root = new File(home + "/Dropbox/spring-cast/0-PUBLISHED/2020/dr-dave-syer-10-10-2019/output/");
+		var root = new File(home + "/Dropbox/spring-cast/0-PUBLISHED/2019/simon-baslé-10-21-2019/");
 		var intro = new File(root, "intro.mp3");
 		var interview = new File(root, "interview.mp3");
-		var photo = new File(root, "dave-and-i.jpg");
-		Assert.isTrue(intro.exists() && interview.exists(),
+		var photo = new File(root, "simon-basle-image.jpg");
+		Assert.isTrue(intro.exists() && interview.exists() && photo.exists(),
 				"the sample files " + intro.getAbsolutePath() + " and " + interview.getAbsolutePath());
 		var sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss.SSS");
 		var text2 = "Josh Long (@starbuxman) talks to another amazing guest @ " + sdf.format(new Date());
