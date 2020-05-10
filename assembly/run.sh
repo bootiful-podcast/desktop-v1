b7799464e98f862a681dd347969cf97352752ffd
Josh Long <josh@joshlong.com>
2020-05-09 23:36:22 -0700

    polish Sat May  9 23:36:22 PDT 2020

--- assembly/run.sh
+++ assembly/run.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-
+LOG=$HOME/Desktop/bootiful-podcast.log
 
 do_run(){
     export JAVA_HOME=${HOME}/.sdkman/candidates/java/current
     DIRECTORY_OF_APP=$(dirname $0)
-    LOG=$HOME/Desktop/bootiful-podcast.log
     env
     echo "the current directory is ${DIRECTORY_OF_APP}."
     echo "starting.."
