cc2a0a46bea648e67138208445a4862c32aebadd
Josh Long <josh@joshlong.com>
2020-05-09 23:42:47 -0700

    polish Sat May  9 23:42:47 PDT 2020

--- assembly/run.sh
+++ assembly/run.sh
@@ -1,15 +1,11 @@
 #!/usr/bin/env bash
-LOG=$HOME/Desktop/bootiful-podcast.log
 
-do_run(){
-    export JAVA_HOME=${HOME}/.sdkman/candidates/java/current
-    DIRECTORY_OF_APP=$(dirname $0)
-    env
-    echo "the current directory is ${DIRECTORY_OF_APP}."
-    echo "starting.."
-    echo $0
-    export SPRING_PROFILES_ACTIVE="$(cat bp_mode)"
-    ${DIRECTORY_OF_APP}/desktop.jar
-}
-do_run > $LOG 2>&1
+LOG=$HOME/Desktop/bootiful-podcast.log
+export JAVA_HOME=${JAVA_HOME:-${HOME}/.sdkman/candidates/java/current}
+DIRECTORY_OF_APP=$(dirname $0)
+echo "the current directory is ${DIRECTORY_OF_APP}."
+echo "starting.."
+echo $0
+export SPRING_PROFILES_ACTIVE="$(cat bp_mode)"
+${DIRECTORY_OF_APP}/desktop.jar
 disown
