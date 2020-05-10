ac0c2a7b9a609b522e55165d8afc1d08e231f7c9
Josh Long <josh@joshlong.com>
2020-05-10 16:37:49 -0700

    polish Sun May 10 16:37:49 PDT 2020

--- build-osx.sh
+++ build-osx.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env bash
 
 ## Builds the OSX .app
-
-#rm -rf target
-#mvn -DskipTests=true spring-javaformat:apply package
+export BP_MODE=${BP_MODE:-development}
+mkdir -p target/macos
 HERE=$(cd `dirname $0` && pwd )
 T=target
 app_name=BootifulPodcast
-app=target/${app_name}.app
+app=target/macos/${app_name}.app
 contents=${app}/Contents/
 mac_os=${contents}/MacOS
 resources=${contents}/Resources
 mkdir -p $mac_os
 mkdir -p $resources
 
-echo ${BP_MODE:-development} > ${mac_os}/bp_mode
+echo ${BP_MODE} > ${mac_os}/bp_mode
 cp -r ${HERE}/assembly/run.sh ${mac_os}/run.sh
 cp -r ${HERE}/target/desktop.jar ${mac_os}/desktop.jar
 cp -r ${HERE}/assembly/app.icns ${resources}/app.icns
@@ -22,7 +21,7 @@ cp -r ${HERE}/assembly/Info.plist ${contents}/Info.plist
 chmod +x $app
 touch $app
 
-archive_name=${app}.tgz
+archive_name=${app}-${BP_MODE}.tgz
 
 tar -c  $app  | gzip -9 > $archive_name
 echo "the application has been saved into ${archive_name}."
